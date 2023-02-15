fdf0040a4644fd74ea46b9856f768c1933ceaeb5
nahunhee <nahunhee@nate.com>
2023-02-15 09:39:33 +0900

    up

---
+++ story_content/user.js
@@ -0,0 +1,16 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6FP8vPSrO0p":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  console.log("time");
+console.log(time)
+}
+
