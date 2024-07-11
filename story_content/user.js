e361366d221f9d090b97a35f9935378132060109
nahunhee <nahunhee@nate.com>
2024-07-11 05:30:25 -0500

    20240711

---
+++ story_content/user.js
@@ -0,0 +1,24 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6M4A976BkQ4":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  console.log("time");
+console.log(time)
+}
+
+};
